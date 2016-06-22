7693aa13dc23466e657af9546a5b21db5f8ab1ed
brian <thaletterb@gmail.com>
2016-06-22 00:15:12 -0700

    Preparing old project for move

--- Demo/startup_stm32f10x.d
+++
@@ -1,5 +0,0 @@
-startup_stm32f10x.o: ../startup_stm32f10x.c \
-  /home/brian/Projects/stm/STM32F10x_StdPeriph_Lib_V3.5.0/Libraries/CMSIS/CM3/DeviceSupport/ST/STM32F10x/stm32f10x.h \
-  /home/brian/Projects/stm/STM32F10x_StdPeriph_Lib_V3.5.0/Libraries/CMSIS/CM3/CoreSupport/core_cm3.h \
-  /home/brian/Projects/stm/STM32F10x_StdPeriph_Lib_V3.5.0/Libraries/CMSIS/CM3/DeviceSupport/ST/STM32F10x/system_stm32f10x.h \
-  ../stm32f10x_conf.h
