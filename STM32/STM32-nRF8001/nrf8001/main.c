6e39cc927d1b8adb6f8c69df783fdcfbd2ee02bc
brian <thaletterb@gmail.com>
2016-08-16 00:28:26 -0700

    import template Delay and SysTick_Handler functions

--- STM32/STM32-nRF8001/nrf8001/main.c
+++ STM32/STM32-nRF8001/nrf8001/main.c
@@ -1,16 +1,29 @@
+#include <stm32f10x_spi.h>
 #include "spi.h"
 
-int i = 0;
-int off = 5;
+// Timer code
+static __IO uint32_t TimingDelay;
 
-void inc(void){
-  i += off;
+void Delay(uint32_t nTime){
+    TimingDelay = nTime;
+    while(TimingDelay != 0);
 }
 
+void SysTick_Handler(void){
+// SysTick interrupt handler - Configured to run every 1ms
+
+    // Delay
+    if (TimingDelay != 0x00)
+        TimingDelay--;
+}
 
 int main(void){
-  while (1) {
-    inc();
-  }
+    //Configure SysTick Timer
+    if (SysTick_Config(SystemCoreClock / 1000))
+        while (1);
+
+    while (1) {
+
+    }
 }
 
